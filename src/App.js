5dc014bc355ca3f5b4029edbbee72dbbcd21ed9c
hongyj9823 <hongyj9823@gmail.com>
2022-03-28 00:59:33 +0900

    ADD websocket codes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import {useEffect} from 'react';
 
+function App()
+{
+  useEffect(() =>{
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+
+    ws.onopen = function open() {
+      console.log('connected');
+      ws.send(JSON.stringify({
+        subscribe: ['MSFT']
+      }));
+    };
+    
+    ws.onclose = function close() {
+      console.log('disconnected');
+    };
+    
+    ws.onmessage = function incoming(data) {
+      console.log('comming message')
+      console.log(Yaticker.decode(new Buffer(data.data, 'base64')))
+    };
+  }, []);
+}
 function App() {
   return (
     <div >
